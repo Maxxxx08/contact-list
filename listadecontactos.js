884072235a7305ebc017694be0ed0bbb081d438d
Maxxxx08 <134959384+Maxxxx08@users.noreply.github.com>
2023-07-23 12:38:05 -0600

    Add files via upload

--- listadecontactos.js
+++ listadecontactos.js
@@ -1,5 +1,31 @@
 //lista de contactos
 let listacontactos = ['Juan Perez','Eduardo Lopez','Manuel Jimenez','Roberto Rodriguez','Susana Horia']; //Agregar los contactos//
 
+console.log("Esta es tu lista de contactos");
+console.log(listacontactos);
 
+//añadir contacto
+function nuevocontacto(contacto){
+  console.log("Esta es tu lista de contactos actualizada con el nuevo contacto")
+listacontactos.push(contacto);
+}
+nuevocontacto("Hector Gonzalez");
+console.log(listacontactos);
 
+//borrar contacto
+function eliminarContacto(eliminar) {
+  const indice = listacontactos.indexOf(eliminar);
+  console.log("Esta es tu lista de contactos con el primer contacto eliminado:")
+  if (indice !== -1) {
+    listacontactos.splice(indice, 1);
+}
+}
+eliminarContacto("Juan Perez");
+console.log(listacontactos);
+
+//Imprimir lista de contactos
+console.log("Esta es tu lista de contactos actual:")
+  listacontactos.forEach(function(nombre, array) {
+    
+    console.log(nombre);
+})
