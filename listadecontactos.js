6c3493b2c5c46e777b3aef7076686be0a6686608
Máximo Larios <Maxxxx08@users.noreply.github.com>
2023-07-28 12:29:22 -0600

    Añadi la funcion para editar contacto y tambien el ID de los contactos que lo habia olvidado :/

--- listadecontactos.js
+++ listadecontactos.js
@@ -1,4 +1,4 @@
-//lista de contactos con Informacion
+//lista de contactos con Informacion en Javascript
 console.log("Esta es tu lista de contactos con su informacion respectiva");
 let listacontactos = [
   {
@@ -6,46 +6,53 @@ let listacontactos = [
    apellidos: 'Maradiaga Ortez',
    telefono:'5564897320',
   ubicaciones: ['Tegucigalpa', 'Col.Kennedy'],
+  ID:'1',
   },
 {
-nombres: 'Eduardo Alberto ',
+nombres: 'Eduardo Alberto',
  apellidos: 'Canales Lopez',
 telefono:'779878776',
  ubicaciones: ['San Pedro Sula', 'Col.Planeta'],
+ ID:'2',
+ 
 },
 {
 nombres: 'Manuel David',
  apellidos: 'Jimenez Hernandez',
  telefono: '584958498',
  ubicaciones: ['Choloma ','Brisas del Norte'],
+ ID:'3',
 },
 {
 nombres:'Roberto David',
  apellidos: 'Rodriguez Gonzalez',
  telefono: '85495889',
- ubicaciones: ['Tegucigalpa, El Hato']
+ ubicaciones: ['Tegucigalpa, El Hato'],
+ ID:'4',
 },
 {
-nombres:'David Eduardo ',
+nombres:'David Eduardo',
  apellidos: 'Lopez Gimenez',
  telefono:  '24238492',
-ubicaciones: ['Choluteca','Jocote'] 
+ubicaciones: ['Choluteca','Jocote'] ,
+ID:'5',
 }
 ]; 
 console.log(listacontactos);
 
 //añadir contacto
 console.log("Esta es tu lista de contactos actualizada con el nuevo contacto");
-function agregarcontacto(nombres, apellidos, ubicaciones, telefono) {
+function agregarcontacto(nombres, apellidos, ubicaciones, telefono,id) {
   const nuevo = {
     nombres: nombres,
     apellidos: apellidos,
     ubicaciones: ubicaciones,
     telefono: telefono,
+    ID:id,
   };
   listacontactos.push(nuevo);
 }
-agregarcontacto('Susana Horia', 'Sevelinda Parada', '786767865', 'Tegucigalpa, Pedregal');
+agregarcontacto('Susana Horia', 'Sevelinda Parada', '786767865', 'Tegucigalpa, Pedregal','6');
 console.log(listacontactos);
 
 //borrar contacto
@@ -65,8 +72,33 @@ function contactosactualizados() {
       nombres: ${contacto.nombres}, 
       apellidos: ${contacto.apellidos}, 
       ubicación: ${contacto.ubicaciones}, 
-      teléfono: ${contacto.telefono}`
+      teléfono: ${contacto.telefono}
+      ID:${contacto.ID}`
     );
   }
 }
 contactosactualizados();
+
+// editar un contacto
+function editarcontacto(buscarcontacto, contactoeditado) 
+{
+  listacontactos = listacontactos.map(contactos => {
+    if (contactos.nombres === buscarcontacto) {
+      return { ...contactos, ...contactoeditado 
+      };
+    }
+    return contactos;
+  });
+}
+editarcontacto('Eduardo Alberto',{ //Edite el primer contacto-ID:2
+  nombres: 'Eduardo Halberto',
+  apellidos: 'Canales Loupes',
+  ID:'1',
+}
+);
+
+console.log(listacontactos);
+
+
+
+
